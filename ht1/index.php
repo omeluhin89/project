89cf0be5f9561d865815ae240dd751d79e5183f0
omeluhin89 <omeluhin89@gmail.com>
2019-03-26 20:15:11 +0500

    поправил стилистику кода

--- ht1/index.php
+++ ht1/index.php
@@ -6,27 +6,24 @@
 </head>
 <body>
 <form method="get" action="index.php">
-    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?=@$_GET["user_name"] ?>" />
+    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?= $_GET['user_name'] ?>"/>
     <br>
     <br>
-    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?=@$_GET["age"] ?>" />
+    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?= $_GET['age'] ?>"/>
     <br>
     <br>
     <input type="submit" value="Передать">
 </form>
 <div class="main">
-<p>
-<?php
+    <p>
+        <?php
 
-if (!empty($_GET["user_name"]) && !empty($_GET["age"]))
-{
-    echo "Привет, {$_GET["user_name"]}! Тебе правда  {$_GET["age"]} лет";
-}
+        if (!empty($_GET['user_name']) && !empty($_GET['age'])) {
+            echo "Привет, {$_GET['user_name']}! Тебе правда  {$_GET['age']} лет";
+        }
 
-?>
-</p></div>
-
-
-
+        ?>
+    </p>
+</div>
 </body>
 </html>
