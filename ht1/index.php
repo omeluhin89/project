a5e86f16d478582ad9891c7e39516d8b59f2ae25
omeluhin89 <omeluhin89@gmail.com>
2019-03-27 20:05:44 +0500

    заменил проверку

--- ht1/index.php
+++ ht1/index.php
@@ -1,9 +1,3 @@
-<?php
-if (!isset($_GET['user_name'],$_GET['age'])) {
-    $_GET['user_name'] = '';
-    $_GET['age'] = '';
-}
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -12,10 +6,10 @@ if (!isset($_GET['user_name'],$_GET['age'])) {
 </head>
 <body>
 <form method="get" action="index.php">
-    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?= $_GET['user_name'] ?>"/>
+    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?= $_GET['user_name'] ?? null ?>"/>
     <br>
     <br>
-    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?= $_GET['age'] ?>"/>
+    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?= $_GET['age'] ?? null ?>"/>
     <br>
     <br>
     <input type="submit" value="Передать">
