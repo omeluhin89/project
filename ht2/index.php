dfcceb8015f36555b500fb25725329a674ed66b2
omeluhin89 <omeluhin89@gmail.com>
2019-08-11 23:16:48 +0500

    добавил стили и фиксил правки

--- ht2/index.php
+++ ht2/index.php
@@ -2,45 +2,48 @@
 session_start();
 $_SESSION['user_id'] = 112233;
 if (!empty($_POST['user_name']) && !empty($_POST['age'])) {
-    $user_name = $_POST['user_name'];
-    $user_age = $_POST['age'];
+    $userName = $_POST['user_name'];
+    $userAge = $_POST['age'];
 }
 
+$submit = 'disabled';
+if (isset($_SESSION['user_id'])) {
+    $submit = '';
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Форма</title>
+    <link href="style.css" rel="stylesheet">
 </head>
 <body>
 <div>
     <p>
-        <?php
-        $submit = 'disabled';
-        if (isset($_SESSION['user_id'])) {
-            $submit = '';
-        }
-        if (!empty($user_name) && !empty($user_age)) {
-            echo "Привет, $user_name! Тебе правда $user_age лет";
-            $data = $user_name . ";" . $user_age . "\n";
-            file_put_contents("user.txt", $data, FILE_APPEND);
-        }
-        ?>
+<?php
+    $userName = $_POST['user_name'] ?? null;
+    $userAge = $_POST['age'] ?? null;
+    if ($userName && $userAge) {
+        echo "Привет, $userName! Тебе правда $userAge лет";
+        $data = $userName . ";" . $userAge . "\n";
+        file_put_contents("user.txt", $data, FILE_APPEND);
+    }
+?>
     </p>
 </div>
 <form method="post" action="index.php">
-    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?=$user_name ?? null ?>"/>
+    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?=$userName ?? null ?>"/>
     <br>
     <br>
-    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?=$user_age ?? null ?>"/>
+    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?=$userAge ?? null ?>"/>
     <br>
     <br>
     <input type="submit" value="Передать" <?=$submit ?>>
 </form>
 <p></p>
-<div style="background: #cccccc; border: 2px solid black; border-radius: 9px; width: 150px;">
-    <?php
+<div class="user-list">
+<?php
     $text = file_get_contents('user.txt');
     $usersDataList = explode("\n", $text);
     array_pop($usersDataList);
@@ -48,7 +51,7 @@ if (!empty($_POST['user_name']) && !empty($_POST['age'])) {
         [$name, $age] = explode(';', $value);
         echo "$name, $age лет<br>";
     }
-    ?>
+?>
 </div>
 </body>
 </html>
