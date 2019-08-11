e52435f7badd9cf5ed0b7f4b62276c7914785bfe
omeluhin89 <omeluhin89@gmail.com>
2019-08-11 23:26:03 +0500

    добавил стили и фиксил правки

--- ht2/index.php
+++ ht2/index.php
@@ -1,7 +1,9 @@
 <?php
 session_start();
 $_SESSION['user_id'] = 112233;
-if (!empty($_POST['user_name']) && !empty($_POST['age'])) {
+$userName = $_POST['user_name'] ?? null;
+$userAge = $_POST['age'] ?? null;
+if ($userName && $userAge) {
     $userName = $_POST['user_name'];
     $userAge = $_POST['age'];
 }
@@ -19,29 +21,27 @@ if (isset($_SESSION['user_id'])) {
     <link href="style.css" rel="stylesheet">
 </head>
 <body>
-<div>
-    <p>
-<?php
-    $userName = $_POST['user_name'] ?? null;
-    $userAge = $_POST['age'] ?? null;
-    if ($userName && $userAge) {
-        echo "Привет, $userName! Тебе правда $userAge лет";
-        $data = $userName . ";" . $userAge . "\n";
-        file_put_contents("user.txt", $data, FILE_APPEND);
-    }
-?>
-    </p>
-</div>
 <form method="post" action="index.php">
-    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?=$userName ?? null ?>"/>
+    Имя <input name="user_name" type="text" maxlength="25" size="20" value="<?=$userName ?>"/>
     <br>
     <br>
-    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?=$userAge ?? null ?>"/>
+    Возраст <input name="age" type="text" maxlength="2" size="3" value="<?=$userAge ?>"/>
     <br>
     <br>
     <input type="submit" value="Передать" <?=$submit ?>>
 </form>
 <p></p>
+<div>
+    <p>
+        <?php
+        if (!empty($userName) && !empty($userAge)) {
+            echo "Привет, $userName! Тебе правда $userAge лет";
+            $data = $userName . ";" . $userAge . "\n";
+            file_put_contents("user.txt", $data, FILE_APPEND);
+        }
+        ?>
+    </p>
+</div>
 <div class="user-list">
 <?php
     $text = file_get_contents('user.txt');
